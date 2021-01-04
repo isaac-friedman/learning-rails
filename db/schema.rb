e7e2e7bd7781d5a82e089e64aca2b5789e770fe0
Isaac Friedman <isaac@isaac-friedman.com>
2021-01-04 15:39:38 -0500

    comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_213906) do
+ActiveRecord::Schema.define(version: 2021_01_04_203656) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
