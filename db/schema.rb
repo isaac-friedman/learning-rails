503519471d4a0d9621e58c2df13ddb9c8e4f8287
Isaac Friedman <isaac@isaac-friedman.com>
2021-01-05 13:49:56 -0500

    Comments complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_203656) do
+ActiveRecord::Schema.define(version: 2021_01_04_204419) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_01_04_203656) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
